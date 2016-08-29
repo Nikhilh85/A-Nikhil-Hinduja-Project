2180f0100480bb1cc2b1d5573ef20f4e3c4d96d8
Nikhilh85 <Nikhilhinduja85@gmail.com>
2016-08-29 17:40:51 +0530

    Add files via upload

---
+++ Nikhil(length of name).py
@@ -0,0 +1,9 @@
+while True :
+    name= input ("enter your name:")
+    ans= input("do you want to continue(y/n):")
+    if ans == "n":
+        break
+    print("length of name=",len(name))
+
+print("bye")
+    
